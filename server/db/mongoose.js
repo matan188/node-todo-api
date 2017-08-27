77cb0012ac1dc64b15961e479419f32b86ccf240
Matan <matan.modai@gmail.com>
2017-08-27 17:26:30 +0300

    Added workaround for mongoose deprecation warning

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 // MONGOOSE SETUP
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp', { useMongoClient: true});
 
 module.exports = {mongoose};
