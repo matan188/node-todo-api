ab15068d91b8b52074fd80bebd10777fe097e236
Matan <matan.modai@gmail.com>
2017-09-03 11:44:12 +0300

    Tests: DELETE /todos/:id route

--- server/tests/server.tests.js
+++ server/tests/server.tests.js
@@ -107,3 +107,43 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo by id', (done) => {
+        var hexId = todos[1]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if (err){
+                    return done(err);
+                }
+
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toNotExist();
+                    done();
+                }).catch((e) => {
+                    return done(e);
+                });
+            });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var hexId = (new ObjectID()).toHexString();
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 if object id is not valid', (done) => {
+        request(app)
+        .delete(`/todos/123`)
+        .expect(404)
+        .end(done);
+    });
+});
