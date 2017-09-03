0cb9447460ddc40b5b675b69ea1849f0a53c7b79
Matan <matan.modai@gmail.com>
2017-09-03 12:42:58 +0300

    Fix: PATCH /todos/:id update completedAt for todo when completed changes to true

--- server/server.js
+++ server/server.js
@@ -79,7 +79,7 @@ app.patch('/todos/:id', (req, res) => {
   }
 
   if (_.isBoolean(body.completed) && body.completed) {
-    body.completed = true;
+    body.completedAt = new Date().getTime();
   } else {
     body.completed = false;
     body.completedAt = null;
